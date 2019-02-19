50d3bc611cd11c23809d2eaf072e6122574b0d10
pet-ro <peter.robisch@gmx.net>
2019-02-19 16:06:14 +0100

    save app module, component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './component/home/home.component';
+//       Product Page related
 import { ProductListComponent } from './component/product-view/product-list/product-list.component';
 import { ProductDetailsComponent } from './component/product-view/product-details/product-details.component'; 
+
+//       Admin related
+import { AdminPageComponent } from './component/admin-page/admin-page.component'
+//             Admin: user login and registration        
+import { LoginFormComponent } from './component/admin-view/login-form/login-form.component'
+import { RegisterFormComponent } from './component/admin-view/register-form/register-form.component'
+//             Admin: Product Book entries
+import { AdminProductBookComponent}  from './component/admin-view/admin-product-book/admin-product-book.component';
+import { from } from 'rxjs';
 
 const routes: Routes = [
    { path: '', redirectTo: 'home', pathMatch: 'full'},
    { path: 'home', component: HomeComponent},
+
+   // view Products via ProductPage
+   // provide a listing and a detail view
    { path: 'product-page', redirectTo: 'product', pathMatch: 'full' },
    { path: 'product',      component: ProductListComponent},
-   { path: 'product/:key', component: ProductDetailsComponent}
+   { path: 'product/:key', component: ProductDetailsComponent},
+
+   // view  AdminPage
+   { path: 'admin', component: AdminPageComponent, pathMatch: 'full' },
+   //       The menu on AdminPage gives access to
+   { path: 'login', component: AdminProductBookComponent },
+   { path: 'register', component: AdminProductBookComponent },
+   { path: 'admin-product-book', component: AdminProductBookComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,61 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule  } from '@angular/common/http'; 
 //  App uses Routing
 import { AppRoutingModule } from './app-routing.module';
+// App uses template-driven forms
+import { FormsModule } from '@angular/forms'
 
 // THE APP
 import { AppComponent } from './app.component';
 
-//  Views
-//      Home is a kind of menu
+//      Home
+//      Home is a kind of main menu
 import { HomeComponent } from './component/home/home.component';
 
 //      Product page 
 import { ProductPageComponent } from './component/product-page/product-page.component';
-import { ProductProviderService } from './service/product-provider.service';
+import { ProductProviderService } from './service/product/product-provider.service';
 import { ProductListComponent } from './component/product-view/product-list/product-list.component';
 import { ProductDetailsComponent } from './component/product-view/product-details/product-details.component';
 
 // Reactive Programming
 import { from } from 'rxjs';
+import { PublisherComponent } from './demo/rx/publish-subscribe/publisher/publisher.component';
+import { SubscriberComponent } from './demo/rx/publish-subscribe/subscriber/subscriber.component';
+import { SearchComponent } from './component/search/search.component';
+
+// Edit book entry
+import { AdminPageComponent } from './component/admin-page/admin-page.component';
+import { AdminProductBookComponent } from './component/admin-view/admin-product-book/admin-product-book.component';
+import { LoginFormComponent } from './component/admin-view/login-form/login-form.component';
+import { RegisterFormComponent } from './component/admin-view/register-form/register-form.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    // app-related
+    AdminPageComponent,
+    AdminProductBookComponent,
+    // app-related
     HomeComponent,
+    // app-related
     ProductPageComponent,
     ProductListComponent,
     ProductDetailsComponent,
+    // app-related
+    SearchComponent,
+    // demo 
+    PublisherComponent,
+    SubscriberComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule, 
     HttpClientModule, // Note: HttpClientModule requires load after BrowserModul 
     AppRoutingModule,
+    FormsModule,
 
   ],
   providers: [

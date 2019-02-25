4947b12f2162f0d32ec79731951acdc76bac7c29
pet-ro <peter.robisch@gmx.net>
2019-02-25 16:29:18 +0100

    made property eatured optional

--- src/app/alfa/model/type/product-item.ts
+++ src/app/alfa/model/type/product-item.ts
@@ -9,7 +9,7 @@ export class ProductItem {
      public title: string,  
      public shortDescriptions: string[],
      public inStock?: number,
-     public featured: boolean,
+     public featured?: boolean,
      public description?: string[],
      public price?: number,
      public thumbnails?: Thumbnail[],

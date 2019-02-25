d2487423f8f5a3927359618332e41b936b6971c9
pet-ro <peter.robisch@gmx.net>
2019-02-25 11:53:22 +0100

    menu adapted layout

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  title = 'BookMonkey (alfatraining project) '
+  title = 'alfa (Produkte & Dienstleistung) '
 
   constructor() { }
 
